4ba41e9f02eb9c3e97d99ff5bf9e3ec21b833e9f
Guillaume Desforges <aceus02@gmail.com>
2019-12-12 11:41:29 +0100

    Download both complete and small datasets

--- download_data.py
+++ download_data.py
@@ -6,25 +6,29 @@ except ImportError:
     from urllib import urlretrieve
 
 
-DATA_URL = (
-    "https://drive.google.com/uc?export=download&id=1c6x1w2KC3PDpNOsUMq9V_ASMpCmJg3or"
-)
-OUTPUT_NAME = "who_wrote_this_corpus.csv"
+DATA_URLS = {
+    "who_wrote_this_corpus_complete.csv": "https://drive.google.com/uc?export=download&id=1c6x1w2KC3PDpNOsUMq9V_ASMpCmJg3or",
+    "who_wrote_this_corpus_small.csv": "https://drive.google.com/uc?export=download&id=1pzF0xsIEeknAJjovi4iNXFcJKzlsxWFR",
+}
 
 
 def main(output_dir="data"):
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
 
-    output_file = os.path.join(output_dir, OUTPUT_NAME)
+    for file_name, url in DATA_URLS.items():
+        output_file = os.path.join(output_dir, file_name)
+        print(f"Downloading {output_file}")
 
-    if os.path.exists(output_file):
-        print("Data already downloaded, delete it if you want to download it again.")
-        return
+        if os.path.exists(output_file):
+            print(
+                "Data already downloaded, delete it if you want to download it again."
+            )
+            continue
 
-    print("Downloading from {} ...".format(DATA_URL))
-    urlretrieve(DATA_URL, filename=output_file)
-    print("=> File saved as {}".format(output_file))
+        print("Downloading from {} ...".format(url))
+        urlretrieve(url, filename=output_file)
+        print("=> File saved as {}".format(output_file))
 
 
 if __name__ == "__main__":
