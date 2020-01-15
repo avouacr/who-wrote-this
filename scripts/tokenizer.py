0b5729362de213fb947c2896c3ab11c4b9854328
JCCen <jaimecostacentena@gmail..com>
2020-01-15 16:42:39 +0100

    refactored tokenizer script

--- scripts/tokenizer.py
+++ scripts/tokenizer.py
@@ -1,18 +1,14 @@
 import argparse
-
-from functools import reduce
+import json
 from collections import Counter
-
-import numpy as np
-import pandas as pd
-import regex as re # needed as re does not allow infinite lookbacks
+from functools import reduce
 
 import nltk
-from nltk.tokenize import word_tokenize
+import numpy as np
+import pandas as pd
+import regex as re  # needed as re does not allow infinite lookbacks
 from nltk.corpus import stopwords
-
-import json
-
+from nltk.tokenize import word_tokenize
 
 # Custom stopwords
 CUSTOM_STOPWORDS = ["--", ".", ",", "!", ";", "’", ":", "?", "...", "'", "«", "»"]
@@ -32,8 +28,10 @@ def make_stopwords_remover(stopwords):
 
     return stopwords_remover
 
+
 def extract_proper_noun(txt):
-    return re.findall(r"(?<!^|\. |\.  )[A-Z][a-z]+",txt)
+    return re.findall(r"(?<!^|\. |\.  )[A-Z][a-z]+", txt)
+
 
 def tokenize_data(df, exclude_stopwords=True, exclude_proper_nouns=True, proper_noun_thresh=7):
     # Prevent side effect
@@ -45,20 +43,20 @@ def tokenize_data(df, exclude_stopwords=True, exclude_proper_nouns=True, proper_
             nltk.data.find("corpora/stopwords.zip")
         except LookupError:
             nltk.download("stopwords")
-    
+
         french_stopwords = set(stopwords.words("french")).union(CUSTOM_STOPWORDS)
-    
+
     if exclude_proper_nouns:
-    # Get proper nouns (persons, places).
-    # We get all names starting with a capital letter and that are not at the
-    # beginning of a sentence. We then remove from the corpus all those that
-    # appear more than proper_noun_thresh times.
+        # Get proper nouns (persons, places).
+        # We get all names starting with a capital letter and that are not at the
+        # beginning of a sentence. We then remove from the corpus all those that
+        # appear more than proper_noun_thresh times.
         proper_nouns = df.paragraph.apply(extract_proper_noun).apply(pd.Series).stack()
-        counted = Counter(proper_nouns) 
+        counted = Counter(proper_nouns)
         filtered_proper_nouns = [el for el in proper_nouns if counted[el] >= proper_noun_thresh]
         filtered_proper_nouns = np.unique(filtered_proper_nouns)
         filtered_proper_nouns = [x.lower() for x in filtered_proper_nouns]
-        
+
     if (exclude_stopwords & exclude_proper_nouns):
         words_to_remove = set(french_stopwords).union(set(filtered_proper_nouns))
     elif exclude_stopwords:
@@ -71,7 +69,7 @@ def tokenize_data(df, exclude_stopwords=True, exclude_proper_nouns=True, proper_
 
     if (exclude_stopwords | exclude_proper_nouns):
         df["tokenized"] = df["tokenized"].map(make_stopwords_remover(words_to_remove))
-       
+
     # Build vocabulary (with word count)
     words_count = reduce(flatten_count, df["tokenized"].values.tolist(), {})
 
